f1bd1a1035c5dd2362292266398bfa8947403607
marciodutra <78557074+marciodutra@users.noreply.github.com>
2024-06-04 13:35:53 -0300

    import do jquery e blockUI

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -8,9 +8,19 @@ use App\Models\Produto;
 
 class ProdutosController extends Controller
 {
-    public function index() {
-        $findProduto = Produto::all();
+    public function __construct(Produto $produto)
+    {
+       $this->produto = $produto; 
+    }
+
+    public function index(Request $request) {
+        $pesquisar = $request->pesquisar;
+        $findProduto = $this->produto->getProdutosPesquisarIndex(search: $pesquisar ?? '');
         
         return view('pages.produtos.paginacao', compact('findProduto'));
+    }
+
+    public function delete(Request $request) {
+
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -143,9 +143,12 @@
   </div>
 </div>
 @yield('scripts')
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 <script src="/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js"></script>
 <script src="/js/color-modes.js"></script>
 </script><script src="/js/dashboard.js"></script>
+<script src="/js/projeto.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.blockUI/2.70/jquery.blockUI.js"></script>
 </body>
 </html>
