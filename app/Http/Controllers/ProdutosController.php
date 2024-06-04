dd253083a962970ba83d15d15bb1485bab3b3fa9
marciodutra <78557074+marciodutra@users.noreply.github.com>
2024-06-04 11:52:41 -0300

    Criação página produtos

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Produto;
 
 class ProdutosController extends Controller
 {
     public function index() {
-        return 'produtos';
+        $findProduto = Produto::all();
+        
+        return view('pages.produtos.paginacao', compact('findProduto'));
     }
 }
