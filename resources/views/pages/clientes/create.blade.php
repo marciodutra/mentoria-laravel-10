84389d8cae6b89ff68d96ded35ce89365e9d9843
marciodutra <78557074+marciodutra@users.noreply.github.com>
2024-06-09 16:04:45 -0300

    Busca api via cep

--- resources/views/pages/clientes/create.blade.php
+++ resources/views/pages/clientes/create.blade.php
@@ -30,21 +30,21 @@
           </div>
           <div class="mb-3">
             <label  class="form-label">Endereço</label>
-            <input value="{{ old('endereco') }}" class="form-control @error('endereco') is-invalid @enderror" name="endereco">
+            <input id="endereco" value="{{ old('endereco') }}" class="form-control @error('endereco') is-invalid @enderror" name="endereco">
             @if ($errors->has('endereco'))
               <div class="invalid-feedback">{{ $errors->first('endereco') }}</div>
             @endif
           </div>
           <div class="mb-3">
             <label  class="form-label">Logradouro</label>
-            <input value="{{ old('logradouro') }}" class="form-control @error('logradouro') is-invalid @enderror" name="logradouro">
+            <input id="logradouro" value="{{ old('logradouro') }}" class="form-control @error('logradouro') is-invalid @enderror" name="logradouro">
             @if ($errors->has('logradouro'))
               <div class="invalid-feedback">{{ $errors->first('logradouro') }}</div>
             @endif
           </div>
           <div class="mb-3">
             <label  class="form-label">Bairro</label>
-            <input value="{{ old('bairro') }}" class="form-control @error('bairro') is-invalid @enderror" name="bairro">
+            <input id="bairro" value="{{ old('bairro') }}" class="form-control @error('bairro') is-invalid @enderror" name="bairro">
             @if ($errors->has('bairro'))
               <div class="invalid-feedback">{{ $errors->first('bairro') }}</div>
             @endif
